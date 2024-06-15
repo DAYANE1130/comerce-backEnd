e389aa2c62fbe019d30bc06dc22664d9f28e1323
Dayane <dayanebmartins@gmail.com>
2024-06-14 23:16:38 -0300

    feat(Backend- Entidade -Categoria): Implementa funções  de crud da tabela categorias na camada controller

---
+++ motoca_systems/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\CategoryService;
+use App\Http\Requests\CategoryRequest;
+
+class CategoryController extends Controller
+{
+    
+    private $categoryService;
+    public function __construct(CategoryService $categoryService)
+    {
+        $this->categoryService = $categoryService;
+    }
+/**
+     * Display a listing of the resource.
+     */
+
+    public function index()
+    {
+        $responseService = $this->categoryService->getAllCategories();
+        return response()->json($responseService);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CategoryRequest $request)
+    {
+        {
+            try {
+                $category = $this->categoryService->createCategory($request->all());
+                return response()->json($category, 201);
+            } catch (\Exception $e) {
+                return response()->json(['error' => $e->getMessage()], 400);
+            }
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        try {
+            $responseService = $this->categoryService->getCategoryById($id);
+            return response()->json($responseService);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CategoryRequest $request, string $id)
+    {
+        try {
+            $responseService =  $this->categoryService->updateCategory($id, $request->all());
+            return response()->json([$responseService,'message' => 'Category updated sucessfull']);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        try {
+            $this->categoryService->deleteCategory($id);
+            return response()->json(['message' => 'Category deleted sucessfull']);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+    }
+}
